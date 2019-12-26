1231a29aea0d0516855be69d63188ead3f23e960
valosekj <jan.valosek@upol.cz>
2019-12-26 08:41:01 +0100

    config_bash.sh script integration

---
+++ config_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script for enviroment configuration
+
+# Jan Valosek, fMRI laboratory, Olomouc, 2019-2020
+# VER=26-12-2019
+
+# Add basic bash functions into PATH variable
+export PATH=$PATH:/md1/ing/bin/bash_basics
+
+# Set path to matlab scripts
+export MATLAB_SCRIPT=/md1/bin/matlab_scripts/
