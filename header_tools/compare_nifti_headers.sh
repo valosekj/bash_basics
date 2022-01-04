88fc0a7864c276d29ff19f456124a456536e3ab7
valosekj <jan.valosek@upol.cz>
2022-01-04 12:23:41 +0100

    Update CLI help

--- header_tools/compare_nifti_headers.sh
+++ header_tools/compare_nifti_headers.sh
@@ -5,8 +5,8 @@
 
 if [[ $1 == "" ]] || [[ $1 == "--help" ]];then
   echo "Compare nifti header of two files"
-  echo -e "\nUSAGE:\n\tcompare_nifti_headers.sh <file1> <file2>"
-  echo -e "EXAMPLE:\n\tcompare_nifti_headers.sh T1_run-01.nii.gz T1_run-02.nii.gz"
+  echo -e "\nUSAGE:\n\t${0##*/} <file1> <file2>"
+  echo -e "EXAMPLE:\n\t${0##*/} T1_run-01.nii.gz T1_run-02.nii.gz"
   exit
 fi
 
