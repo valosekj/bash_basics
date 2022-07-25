3980b1f7b92484fa1e8e088e25bcbb72e32b0933
valosekj <jan.valosek@upol.cz>
2022-07-25 13:35:54 +0200

    Add tests for get_unique_bvals shell script

--- tests/tests.py
+++ tests/tests.py
@@ -10,9 +10,9 @@ import io
 import subprocess
 
 
-def read_bval_file(bval):
+def run_count_bvals(bval):
     """
-    Read test bval file from tests directory
+    Run count_bvals.py script and read test bval file from tests directory
     :return:
     """
     # Get the abs path where is this script located
@@ -28,11 +28,28 @@ def read_bval_file(bval):
     return proc
 
 
+def run_get_unique_bvals():
+    """
+    Run get_unique_bvals shell script and read test bval file from tests directory
+    :return:
+    """
+    # Get the abs path where is this script located
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    # Call python script
+    proc = subprocess.Popen(
+        [os.path.join(script_path, '../dwi_tools/get_unique_bvals'),
+         os.path.join(script_path, 'sub-001_dwi.bval')],
+        stdout=subprocess.PIPE
+    )
+
+    return proc
+
+
 def test_count_bvals_1000():
     """
     Count b-values equal to 1000
     """
-    proc = read_bval_file('1000')
+    proc = run_count_bvals('1000')
     # Read std output
     for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
         assert line == '3\n'       # \n - new line
@@ -42,7 +59,7 @@ def test_count_bvals_550():
     """
     Count b-values equal to 550
     """
-    proc = read_bval_file('550')
+    proc = run_count_bvals('550')
     # Read std output
     for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
         assert line == '2\n'       # \n - new line
@@ -52,7 +69,7 @@ def test_count_bvals_0():
     """
     Count b-values equal to 0
     """
-    proc = read_bval_file('0')
+    proc = run_count_bvals('0')
     # Read std output
     for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
         assert line == '2\n'       # \n - new line
@@ -62,7 +79,17 @@ def test_count_bvals_100():
     """
     Count b-values equal to 100
     """
-    proc = read_bval_file('100')
+    proc = run_count_bvals('100')
     # Read std output
     for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
         assert line == '0\n'       # \n - new line
+
+
+def test_get_uniqe_bvals():
+    """
+    Get unique bvals
+    """
+    proc = run_get_unique_bvals()
+    # Read std output
+    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+        assert line == '0 550 1000 \n'       # \n - new line
