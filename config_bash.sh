c999b2d479728a46150e6c1f0299fe56eb8d2dde
valosekj <jan.valosek@upol.cz>
2024-05-24 07:14:38 -0400

    Add path to nii_tools

--- config_bash.sh
+++ config_bash.sh
@@ -13,6 +13,6 @@ path_to_repo="$HOME/code"
 source ${path_to_repo}/bash_basics/bash_basic_functions.sh
 # Add scripts into PATH variable
 export PATH=${PATH}:${path_to_repo}/bash_basics/dwi_tools
+export PATH=${PATH}:${path_to_repo}/bash_basics/nii_tools
 export PATH=${PATH}:${path_to_repo}/bash_basics/header_tools
 export PATH=${PATH}:${path_to_repo}/bash_basics/other_tools
-
