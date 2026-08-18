d8793eb066d13e9b2b8e42abaa63479a2cafd155
valosekj <jan.valosek@upol.cz>
2026-08-18 16:08:16 +0200

    Make remove_spaces_and_dots.py executable and support wildcard file args

--- other_tools/remove_spaces_and_dots.py
+++ other_tools/remove_spaces_and_dots.py
@@ -1,20 +1,32 @@
+#!/usr/bin/env python3
 """
-Replace spaces by underscores (' ' -> '_') and dots by dashes ('.' -> '-') in filenames of all files in the input directory
+Replace spaces by underscores (' ' -> '_') and dots by dashes ('.' -> '-') in filenames.
 
 USAGE:
 	python remove_spaces_and_dots.py /path/to/directory
+	python remove_spaces_and_dots.py /path/to/directory/*
 """
 
 import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("path", help="Input path")
+parser.add_argument("path", nargs="+", help="Input directory, or files (e.g. a shell '*' glob)")
 args = parser.parse_args()
 
-path = args.path
 
-for filename in os.listdir(path):
-    if os.path.isfile(os.path.join(path, filename)):
-        new_filename = filename.replace(" ", "_").replace(".", "-", filename.count(".")-1)
-        os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
+def rename_file(filepath):
+    directory, filename = os.path.split(filepath)
+    new_filename = filename.replace(" ", "_").replace(".", "-", filename.count(".") - 1)
+    if new_filename != filename:
+        os.rename(filepath, os.path.join(directory, new_filename))
+
+
+for path in args.path:
+    if os.path.isdir(path):
+        for filename in os.listdir(path):
+            filepath = os.path.join(path, filename)
+            if os.path.isfile(filepath):
+                rename_file(filepath)
+    elif os.path.isfile(path):
+        rename_file(path)
